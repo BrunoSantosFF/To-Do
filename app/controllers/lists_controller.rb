8abccb71c48cf40cd344566b2100eb4589a6fecb
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-10-17 16:00:40 -0300

    adicionando funções list_controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,62 @@
 class ListsController < ApplicationController
+  # Callback para chamar a função antes das outras
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  # Função para chamar todas as listas do banco de dados
   def index
+    @lists = List.all
   end
 
+  # Função para mostrar os itens na pagina de Show 
   def show
+    @item = Item.new
   end
 
+  # Função para criar a lista vazia e renderizar a pagina New
   def new
+    @list = List.new
   end
 
+  # Função que cria a lista através dos parametros
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path, notice: "Lista criada com sucesso !!!!!"
+    else
+      render :new
+    end
   end
 
+  # Página de editar
   def edit
   end
 
+  # Responsável por editar a lista
   def update
+    if @list.update(list_params)
+      redirect_to list_path(@list), notice: "Lista editada com sucesso !!!!"
+    else 
+      render :edit
+    end
   end
 
+  # Responsável por destruir a lista
   def destroy
+    @list.destroy
+    redirect_to lists_path, notice: "Lista deletada com sucesso!"
   end
+
+  # Funçoes privadas 
+  private
+
+  # Encontrando o item pelo id nos parametros
+  def set_list
+    @list = List.find(params[:id]) 
+  end
+
+  # Responsável por filtrar os parâmetros que estão sendo enviados
+  def list_params
+    params.require(:list).permit(:name)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/create'
-  get 'items/update'
-  get 'items/destroy'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/create'
-  get 'lists/edit'
-  get 'lists/update'
-  get 'lists/destroy'
+  resources :lists
+  resources :items
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
